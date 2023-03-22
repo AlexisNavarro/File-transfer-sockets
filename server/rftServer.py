fa693893142f160b5b174bcdbf5cf06b2fde52b5
alexis <navarroalexis007@gmail.com>
2023-03-22 12:35:40 -0600

    made it possible to send the file to the server

--- server/rftServer.py
+++ server/rftServer.py
@@ -1,4 +1,5 @@
 from socket import *
+import os
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -13,9 +14,35 @@ while True:
     print("receiving request")
     data = client.recv(1024)
 
-    print(data)
+    #print(data)
+
+    #converting to a string by deconding the byte message
+    message = data.decode()
+    print(message[0:4])
+
+    filename = message.split(" ", 1)
+    
+    #print(filename)
+    if("RETR" == message[0:4]):
+        print("RETR request received")
+
+        #check file here make it later
+
+        file = open(filename[1],"rb")
+     
+        #send the content of the file to the client
+
+        file_data = file.read(1024)
+        
+        while file_data:
+            client.send(file_data)
+            file_data = file.read(1024)
+        #client.sendall(file_data)
+
     break
 
-
+file.close()
 client.close()
+serverSocket.close()
+
 
