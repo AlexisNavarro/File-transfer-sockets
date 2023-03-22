a466165e7b371da60240af08d4990b14e02e7dc0
alexis <navarroalexis007@gmail.com>
2023-03-22 12:35:11 -0600

    made it able to retrieve the file from the server

--- client/rftClient.py
+++ client/rftClient.py
@@ -1,18 +1,40 @@
 from socket import *
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
 
-#requesting the user to give the destination where our files will be sent to.
-ip_addr = input("Please provide a destination IP address to connect: ")
-port = input("Please provide a destination port number to connect: ")
+def ret_text_file(clientSocket, file_name):
+    file_recv = open(file_name,"wb")
 
-server_port = int(port) 
-clientSocket.connect((ip_addr, server_port))
-#clientSocket.send(b"Hello server")
+    line = clientSocket.recv(1024)
 
-print("YOU HAVE CONNECTED")
+    while line:
+        file_recv.write(line)
+        line = clientSocket.recv(1024)
 
-request_file = input("please type the file name you want to retrieve using RETR at the start: ")
 
-send_req = request_file.encode()
-clientSocket.send(send_req)
+    print("rcv successful")
+
+def main():
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+
+    #requesting the user to give the destination where our files will be sent to.
+    ip_addr = input("Please provide a destination IP address to connect: ")
+    port = input("Please provide a destination port number to connect: ")
+
+    server_port = int(port) 
+    clientSocket.connect((ip_addr, server_port))
+  
+    print("YOU HAVE CONNECTED")
+
+    request_file = input("please type the file name you want to retrieve using RETR at the start: ")
+
+    send_req = request_file.encode()
+    clientSocket.send(send_req)
+
+    file_name = request_file[4:]
+   
+    #if request_file[-3:] == "txt":
+    ret_text_file(clientSocket,file_name)
+    #if request_file[-3:] == "png"
+
+if __name__ == '__main__':
+    main()
