c32eed7f9e4a090d049b9c813bcbeafd06c9b42b
alexis <navarroalexis007@gmail.com>
2023-03-26 14:39:17 -0600

    added comments to the file

--- client/rftClient.py
+++ client/rftClient.py
@@ -1,19 +1,19 @@
 from socket import *
 
-
+#method where we receive the file from the server
 def ret_file(clientSocket, file_name):
     file_recv = open(file_name,"wb")
 
+    #recv first 1000 bytes of the files contents
     line = clientSocket.recv(1000)
-    #print(line)
+    
     if line == b"file not found":
        print(line)
     else:
+        #ensure that we get every packet of the file that is being recieved
         while line:
             file_recv.write(line)
             line = clientSocket.recv(1000)
-
-
         print("rcv successful")
 
 def main():
@@ -25,7 +25,6 @@ def main():
 
     server_port = int(port) 
     clientSocket.connect((ip_addr, server_port))
-  
     print("YOU HAVE CONNECTED")
 
     request_file = input("please type the file name you want to retrieve using RETR at the start: ")
@@ -33,10 +32,8 @@ def main():
     send_req = request_file.encode()
     clientSocket.send(send_req)
 
+    #create the filename for where the content of the requested file will be stored to
     file_name = request_file[4:]
-   
-
-
     ret_file(clientSocket,file_name)
 
 
