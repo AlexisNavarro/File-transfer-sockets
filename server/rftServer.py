de0cc41142c269071575e4f20efc4139e29a0008
alexis <navarroalexis007@gmail.com>
2023-03-26 14:40:06 -0600

    added comments to the file

--- server/rftServer.py
+++ server/rftServer.py
@@ -26,25 +26,26 @@ while True:
     if("RETR" == message[0:4]):
         print("RETR request received")
 
-        #check file here make it later
-
+        #might need to be used for a different implementation if we don't have the correct path set
         #file_location = "server/"+filename[1]
         #print(file_location)
 
+        #using the os import to be able to check the server directory if a file of that name exists
         if os.path.exists(filename[1]) ==  True:
             print("file is found")
             file = open(filename[1],"rb")
         
-
+            #Checking the file size 
             file_size = os.path.getsize(filename[1])
             print(file_size)
+
             #send the content of the file to the client
             file_data = file.read(1000)
             
+            #sending the packets back to back by reading 1000 bytes and then sending those 1000 bytes 
             while file_data:
                 client.send(file_data)
                 file_data = file.read(1000)
-            #client.sendall(file_data)
 
             print("file has been transferred")
             file.close()
